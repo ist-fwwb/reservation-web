6fac84ba35c1a10caf359437bf7a002e779ff105
zzbslayer <zzbslayer@sjtu.edu.cn>
2019-01-19 16:20:55 +0800

    Fix util encapsulation

--- src/variables/general.jsx
+++ src/variables/general.jsx
@@ -3,13 +3,20 @@ const domain = "www.test.com";
 const port = "31000";
 const server = prefix + domain + ":" + port;
 
-const dateToString = (date) => (date.toLocaleDateString().replace(/\//g,'-'));
+const dateToString = (date) => (date.toLocaleDateString([],{year:"numeric", month:"2-digit", day:"numeric"}).replace(/\//g,'-'));
 const today = dateToString(new Date());
 
 const nextDay = (day) => {
   let result = new Date(day);
   result.setDate(result.getDate()+1);
   return dateToString(result);
+}
+
+const idToTime = (id) => {
+  if (id % 2 === 0)
+      return String(id / 2) + ":00";
+  else
+      return String((id - 1 ) / 2) + ":30";
 }
 
 const roomController = {
@@ -21,7 +28,7 @@ const roomController = {
 };
 const timeSliceController = {
   "getTimeSilceByDateAndRoomId": (date, roomId) => (server + "/timeSlice?date=" + date + "&roomId=" + roomId),
-  "getTimeSilceByRoomId": (roomId) => (server + "/timeSlice?&roomId=" + roomId),
+  "getTimeSilceByRoomId": (roomId) => (server + "/timeSlice?roomId=" + roomId),
 };
 
 const meetingController = {
@@ -41,10 +48,11 @@ const userController = {
   "register": () => (server + "/user"), //json params in req body
   "login": (phone, password) => (server + "/user/login?phone=" + phone + "&password=" + password),
   "getUserByUserId": (userId) => (server + "/user/" + userId),
-  "editUserByUserId": (userId) => (server + "/user/" + userId),
+  "editUser": (userId) => (server + "/user/" + userId),
 };
 
 module.exports = {
+  idToTime,
   server,
   today,
   nextDay,
--- src/variables/time.jsx
+++
@@ -1,6 +0,0 @@
-export const time = (id) => {
-    if (id % 2 === 0)
-        return String(id / 2) + ":00";
-    else
-        return String((id - 1 ) / 2) + ":30";
-}
