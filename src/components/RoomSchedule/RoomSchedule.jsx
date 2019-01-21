f4b9a122ce3cff7caaa7fd08a6ce127be2320a08
zzbslayer <zzbslayer@sjtu.edu.cn>
2019-01-22 03:04:06 +0800

    RoomSchedule Refactor

--- src/components/RoomSchedule/RoomSchedule.jsx
+++ src/components/RoomSchedule/RoomSchedule.jsx
@@ -1,25 +1,13 @@
 import React from "react";
 import PropTypes from 'prop-types';
 // core components
-import GridItem from "components/Grid/GridItem.jsx";
-import GridContainer from "components/Grid/GridContainer.jsx";
-import Card from "components/Card/Card.jsx";
-import CardHeader from "components/Card/CardHeader.jsx";
-import CardBody from "components/Card/CardBody.jsx";
-import Snackbar from "components/Snackbar/Snackbar.jsx";
-
-import ErrorOutline from "@material-ui/icons/ErrorOutline";
-import Done from "@material-ui/icons/Done";
 
 import { withStyles } from '@material-ui/core/styles';
-import TextField from '@material-ui/core/TextField';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Fab from '@material-ui/core/Fab';
 
 import pink from '@material-ui/core/colors/pink';
 import orange from '@material-ui/core/colors/orange';
@@ -29,11 +17,9 @@ import teal from '@material-ui/core/colors/teal';
 import green from '@material-ui/core/colors/green';
 
 import { Link } from "react-router-dom";
-import { timeSliceController, meetingController, idToTime } from "variables/general.jsx";
+import { timeSliceController, idToTime } from "variables/general.jsx";
 
 const occupiedMessage = "会议室被占用";
-const timeChosenMessage = "请先选择时间";
-const reservationSuccessMessage = "预约成功";
 
 const colorList = [purple[200], pink[200], orange[300], cyan[200], teal[300]]
 let colorMap = {};
@@ -83,25 +69,13 @@ class RoomSchedule extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      br: false,
-      notificationMessage: "null",
-      notificationType: null,
-
       scheduleData: null,
 
-      date: null,
+      chosenDate: null,
       firstChosen: null,
       secondChosen: null,
     };
   }
-
-  componentWillUnmount() {
-    let id = window.setTimeout(null, 0);
-    while (id--) {
-      window.clearTimeout(id);
-    }
-  }
-
   
   componentDidMount() {
     fetch(timeSliceController.getTimeSilceByRoomId(this.props.roomId), {
@@ -110,48 +84,17 @@ class RoomSchedule extends React.Component{
     })
     .then(res => res.json())
     .then((data) => {
-      if (data.error)
-        this.warning(data.error);
-      else
+      if (data.error){
+        let state = {
+          notificationType: "danger",
+          notificationMessage: data.error
+        };
+        this.props.handleChange(state);
+      }
+      else{
         this.setState({scheduleData: dataToRows(data)})
+      }
     })
-  }
-
-  showNotification = (place) => {
-    let x = [];
-    x[place] = true;
-    this.setState(x);
-    this.alertTimeout = setTimeout(
-      function() {
-        x[place] = false;
-        this.setState(x);
-      }.bind(this),
-      6000
-    );
-  }
-
-  typeToIcon = (type) => {
-    if (type === "success")
-      return Done;
-    if (type === "danger")
-      return ErrorOutline;
-    return null;
-  }
-
-  warning = (msg) => {
-    this.setState({
-      notificationType: "danger",
-      notificationMessage: msg
-    })
-    this.showNotification("br");
-  }
-
-  success = (msg) => {
-    this.setState({
-      notificationType: "success",
-      notificationMessage: msg
-    })
-    this.showNotification("br");
   }
 
   clearBetween = (firstChosen, secondChosen, scheduleData) => {
@@ -188,29 +131,37 @@ class RoomSchedule extends React.Component{
       // state 2 -> state 0
       if (currentCell["click"] || currentCell["between"]){
         scheduleData = this.clearBetween(firstChosen, secondChosen, scheduleData);
-        this.setState({
+        let state = {
           scheduleData: scheduleData,
           firstChosen: null,
           secondChosen: null,
-          date: null
-        })
+          chosenDate: null
+        };
+        this.setState(state);
+        this.props.handleChange(state);
         return;
       }
       else {
         if (scheduleData[x][y]["occupied"]){
-          this.warning(occupiedMessage);
+          let state = {
+            notificationType: "danger",
+            notificationMessage: occupiedMessage
+          };
+          this.props.handleChange(state);
           return;
         }
         // state 2 -> state 1
         scheduleData[x][y]["click"] = true;
         
         scheduleData = this.clearBetween(firstChosen, secondChosen, scheduleData);
-        this.setState({
+        let state = {
           scheduleData: scheduleData,
           firstChosen: [x,y],
-          date: date,
+          chosenDate: date,
           secondChosen: null,
-        })
+        };
+        this.setState(state);
+        this.props.handleChange(state);
         return;
       }
     }
@@ -220,32 +171,44 @@ class RoomSchedule extends React.Component{
 
     else if (!firstChosen){
       if (scheduleData[x][y]["occupied"]){
-        this.warning(occupiedMessage);
+        let state = {
+          notificationType: "danger",
+          notificationMessage: occupiedMessage
+        };
+        this.props.handleChange(state);
         return;
       }
       // state 0 -> state 1
       scheduleData[x][y]["click"] = true;
-      this.setState({
+      let state = {
         scheduleData: scheduleData,
-        date: date,
+        chosenDate: date,
         firstChosen: [x,y]
-      })
+      };
+      this.setState(state);
+      this.props.handleChange(state);
       return;
     }
     // The user click for the second time
     else if (firstChosen === [x,y]){ 
       // state 1 -> state 0
       scheduleData[x][y]["click"] = false;
-      this.setState({
+      let state = {
         scheduleData: scheduleData,
         firstChosen: null,
-      })
+      };
+      this.setState(state);
+      this.props.handleChange(state);
       return;
     }
     else if (firstChosen[1] === y){
       // state 1 -> state 2
       if (scheduleData[x][y]["occupied"]){
-        this.warning(occupiedMessage);
+        let state = {
+          notificationType: "danger",
+          notificationMessage: occupiedMessage
+        };
+        this.props.handleChange(state);
         return;
       }
       // check whether there're timeslice been occupied between them
@@ -261,7 +224,11 @@ class RoomSchedule extends React.Component{
       }
       for (let i = start + 1; i <= end; i ++){
         if (scheduleData[i][y]["occupied"]){
-          this.warning(occupiedMessage);
+          let state = {
+            notificationType: "danger",
+            notificationMessage: occupiedMessage
+          };
+          this.props.handleChange(state);
           return;
         }
       }
@@ -269,243 +236,126 @@ class RoomSchedule extends React.Component{
       for (let i = start + 1; i < end; i ++){
         scheduleData[i][y]["between"] = true;
       }
-      this.setState({
+      let state = {
         scheduleData: scheduleData,
         secondChosen: [x,y],
         notificationType: "success",
         notificationMessage: date + " " + idToTime(start) + " ~ " + idToTime(end+1)
-      })
-      this.showNotification("br");
+      }
+      this.setState(state);
+      this.props.handleChange(state);
       return;
     }
     else{
       if (scheduleData[x][y]["occupied"]){
-        this.warning(occupiedMessage);
+        let state = {
+          notificationType: "danger",
+          notificationMessage: occupiedMessage
+        };
+        this.props.handleChange(state);
         return;
       }
       // state 1 -> state 1
       scheduleData[firstChosen[0]][firstChosen[1]]["click"] = false;
       scheduleData[x][y]["click"] = true;
 
-      this.setState({
+      let state = {
         scheduleData: scheduleData,
-        date: date,
+        chosenDate: date,
         firstChosen: [x,y],
-      })
+      };
+      this.setState(state);
+      this.props.handleChange(state);
       return;
     }
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    let firstChosen = this.state.firstChosen;
-    let secondChosen = this.state.secondChosen;
-    if (!(this.state.firstChosen && this.state.secondChosen)){
-      this.warning(timeChosenMessage);
-      return;
-    }
-    let start;
-    let end;
-    let date = this.state.date;
-    if (!secondChosen){
-      start = firstChosen[0];
-      end = start + 1;
-    }
-    else{
-      start = firstChosen[0];
-      end = secondChosen[0]+1;
-    }
-    
-    let meeting = {
-      "attendantNum": null,
-      "attendants": null,
-      "date": date,
-      "description": "无",
-      "endTime": end,
-      "heading": "Meeting-" + date + "-" + start + "-" + end ,
-      "hostId": this.props.userId,
-      "location": null,
-      "needSignIn": false,
-      "roomId": this.props.roomId,
-      "startTime": start,
-      "type": "COMMON"
-    }
-    meeting = JSON.stringify(meeting);
-    fetch(meetingController.createMeeting(), {
-      credentials: 'include',
-      method: 'post',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: meeting
-    })
-    .then( res => res.json())
-    .then((data) => {
-      if (data.error){
-        this.warning(data.error);
-      }
-      else {
-        this.success(reservationSuccessMessage);
-        window.location.reload();
-      }
-    })
   }
 
   render(){
     let scheduleData = this.state.scheduleData;
-    let { roomLocation } = this.props;
     let colorCount = 0;
-    let timeChosen = "";
-    let firstChosen = this.state.firstChosen;
-    let secondChosen = this.state.secondChosen;
-    if (firstChosen && secondChosen){
-      let start;
-      let end;
-      if (firstChosen[0] > secondChosen[0]){
-        start = secondChosen[0];
-        end = firstChosen[0];
-      }
-      else{
-        end = secondChosen[0];
-        start = firstChosen[0];
-      }
-      timeChosen = this.state.date + " " + idToTime(start) + " ~ " + idToTime(end+1);
-    }
-    else if (firstChosen && !secondChosen){
-      timeChosen = this.state.date + " " + idToTime(firstChosen[0]) + " ~ " + idToTime(firstChosen[0]+1);
-    }
+    
     return(
-      <div>
-        <GridContainer>
-          <GridItem xs={12} sm={12} md={12}>
-            <Card>
-              <CardHeader>
-                <h2>{roomLocation}</h2>
-                <table>
-                  <tbody>
-                    <tr>
-                      <td width={220}>
-                        <TextField
-                          fullWidth
-                          disabled
-                          label="预约时间"
-                          value={timeChosen}
-                          margin="normal"
-                          variant="outlined"
-                        />
-                      </td>
-                      <td>
-                        &nbsp;&nbsp;&nbsp;&nbsp;
-                      </td>
-                      <td>
-                        <Fab variant="extended" color="primary" onClick={this.handleSubmit}>
-                          <Done/>
-                          &nbsp;&nbsp;确定预约
-                        </Fab>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </CardHeader>
-              <CardBody>
-                <Paper>
-                  <Table>
-                    <TableHead>
-                      {
-                        ! scheduleData ? 
-                        <TableRow>
-                          <CustomTableCell>时间</CustomTableCell>
-                          <CustomTableCell align="left">星期一</CustomTableCell>
-                          <CustomTableCell align="left">星期二</CustomTableCell>
-                          <CustomTableCell align="left">星期三</CustomTableCell>
-                          <CustomTableCell align="left">星期四</CustomTableCell>
-                          <CustomTableCell align="left">星期五</CustomTableCell>
-                        </TableRow>
-                        :
-                        <TableRow>
-                          <CustomTableCell>时间</CustomTableCell>
-                          <CustomTableCell align="left">{scheduleData[0][0]["date"]}<br/>星期一</CustomTableCell>
-                          <CustomTableCell align="left">{scheduleData[0][1]["date"]}<br/>星期二</CustomTableCell>
-                          <CustomTableCell align="left">{scheduleData[0][2]["date"]}<br/>星期三</CustomTableCell>
-                          <CustomTableCell align="left">{scheduleData[0][3]["date"]}<br/>星期四</CustomTableCell>
-                          <CustomTableCell align="left">{scheduleData[0][4]["date"]}<br/>星期五</CustomTableCell>
-                        </TableRow>
-                      }                        
-                    </TableHead>
-                    <TableBody>
-                      {! scheduleData ? null : scheduleData.map((row, x) => {
-                        // only display 8:00 ~ 18:00
-                        if (x < 16 || x >= 36){
-                          return null;
-                        }
+      <Table>
+        <TableHead>
+          {
+            ! scheduleData ? 
+            <TableRow>
+              <CustomTableCell>时间</CustomTableCell>
+              <CustomTableCell align="left">星期一</CustomTableCell>
+              <CustomTableCell align="left">星期二</CustomTableCell>
+              <CustomTableCell align="left">星期三</CustomTableCell>
+              <CustomTableCell align="left">星期四</CustomTableCell>
+              <CustomTableCell align="left">星期五</CustomTableCell>
+            </TableRow>
+            :
+            <TableRow>
+              <CustomTableCell>时间</CustomTableCell>
+              <CustomTableCell align="left">{scheduleData[0][0]["date"]}<br/>星期一</CustomTableCell>
+              <CustomTableCell align="left">{scheduleData[0][1]["date"]}<br/>星期二</CustomTableCell>
+              <CustomTableCell align="left">{scheduleData[0][2]["date"]}<br/>星期三</CustomTableCell>
+              <CustomTableCell align="left">{scheduleData[0][3]["date"]}<br/>星期四</CustomTableCell>
+              <CustomTableCell align="left">{scheduleData[0][4]["date"]}<br/>星期五</CustomTableCell>
+            </TableRow>
+          }                        
+        </TableHead>
+        <TableBody>
+          {! scheduleData ? null : scheduleData.map((row, x) => {
+            // only display 8:00 ~ 18:00
+            if (x < 16 || x >= 36){
+              return null;
+            }
 
-                        return(
-                          <TableRow key={x}>
-                            <CustomTableCell component="th" scope="row">
-                              {idToTime(x)+"~"+idToTime(x+1)}
-                            </CustomTableCell>
-                            {
-                              row.map((cell, y) => {
-                                let bgcolor = null;
-                                if (cell["occupied"]){
-                                  let currentid = cell.meetingid;
-                                  if (!colorMap[currentid]){
-                                    colorMap[currentid] = colorList[colorCount];
-                                    colorCount += 1;
-                                    colorCount %= colorList.length;
-                                  }
-                                  bgcolor = colorMap[currentid];
-                                }
-                                else {
-                                  if (cell["click"] || cell["between"]){
-                                    bgcolor = green[400];
-                                  }
-                                }
-                                return (
-                                  <CustomTableCell style={{padding:0}} key={y} align="left" onClick={(e) => {this.handleClick(e, cell["date"], x, y)}}>
-                                  {
-                                    cell["occupied"]?
-                                      <div style={{background:bgcolor ,borderRadius:"15px", lineHeight:"45px" ,height:"45px", width:"92%", textAlign:"center"}}>
-                                        <Link to={"/meeting/"+cell["meetingid"]+"/profile"}>
-                                                  {!cell["name"] ? "null" : (cell["name"].length > 5 ? cell["name"].substring(0,5)+".." : cell["name"])}
-                                        </Link>
-                                      </div>
-                                      :
-                                      <div style={{background:bgcolor ,borderRadius:"15px", lineHeight:"45px" ,height:"45px", width:"92%", textAlign:"center"}}>
-                                      </div>
-                                  }
-                                  </CustomTableCell>
-                                )
-                              })
-                            }
-                          </TableRow>
-                        )
-                      })}
-                    </TableBody>
-                  </Table>
-                  <Snackbar
-                    place="br"
-                    color={this.state.notificationType}
-                    icon={this.typeToIcon(this.state.notificationType)}
-                    message={this.state.notificationMessage}
-                    open={this.state.br}
-                    closeNotification={() => this.setState({ br: false })}
-                    close
-                  />
-                </Paper>
-              </CardBody>
-            </Card>
-          </GridItem>
-        </GridContainer>
-      </div>
+            return(
+              <TableRow key={x}>
+                <CustomTableCell component="th" scope="row">
+                  {idToTime(x)+"~"+idToTime(x+1)}
+                </CustomTableCell>
+                {
+                  row.map((cell, y) => {
+                    let bgcolor = null;
+                    if (cell["occupied"]){
+                      let currentid = cell.meetingid;
+                      if (!colorMap[currentid]){
+                        colorMap[currentid] = colorList[colorCount];
+                        colorCount += 1;
+                        colorCount %= colorList.length;
+                      }
+                      bgcolor = colorMap[currentid];
+                    }
+                    else {
+                      if (cell["click"] || cell["between"]){
+                        bgcolor = green[400];
+                      }
+                    }
+                    return (
+                      <CustomTableCell style={{padding:0}} key={y} align="left" onClick={(e) => {this.handleClick(e, cell["date"], x, y)}}>
+                      {
+                        cell["occupied"]?
+                          <div style={{background:bgcolor ,borderRadius:"15px", lineHeight:"45px" ,height:"45px", width:"92%", textAlign:"center"}}>
+                            <Link to={"/meeting/"+cell["meetingid"]+"/profile"}>
+                                      {!cell["name"] ? "null" : (cell["name"].length > 5 ? cell["name"].substring(0,5)+".." : cell["name"])}
+                            </Link>
+                          </div>
+                          :
+                          <div style={{background:bgcolor ,borderRadius:"15px", lineHeight:"45px" ,height:"45px", width:"92%", textAlign:"center"}}>
+                          </div>
+                      }
+                      </CustomTableCell>
+                    )
+                  })
+                }
+              </TableRow>
+            )
+          })}
+        </TableBody>
+      </Table>
     )
   }
 }
 
 RoomSchedule.propTypes = {
   roomId: PropTypes.string.isRequired,
-  roomLocation: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired
 };
 
 export default RoomSchedule;
--- src/views/RoomSchedulePage/RoomSchedulePage.jsx
+++ src/views/RoomSchedulePage/RoomSchedulePage.jsx
@@ -1,19 +1,220 @@
 import React from "react";
 // core components
+import GridItem from "components/Grid/GridItem.jsx";
+import GridContainer from "components/Grid/GridContainer.jsx";
+import Card from "components/Card/Card.jsx";
+import CardHeader from "components/Card/CardHeader.jsx";
+import CardBody from "components/Card/CardBody.jsx";
+import Snackbar from "components/Snackbar/Snackbar.jsx";
+
+import ErrorOutline from "@material-ui/icons/ErrorOutline";
+import Done from "@material-ui/icons/Done";
+import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Fab from '@material-ui/core/Fab';
+
 import RoomSchedule from "components/RoomSchedule/RoomSchedule.jsx";
+import { meetingController, idToTime } from "variables/general.jsx";
+
+const timeChosenMessage = "请先选择时间";
+const reservationSuccessMessage = "预约成功";
 
 class RoomSchedulePage extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
+      br: false,
+      notificationMessage: "null",
+      notificationType: null,
+
+      firstChosen: null,
+      secondChosen: null
     };
+  }
+
+  showNotification = (place) => {
+    let x = [];
+    x[place] = true;
+    this.setState(x);
+    this.alertTimeout = setTimeout(
+      function() {
+        x[place] = false;
+        this.setState(x);
+      }.bind(this),
+      6000
+    );
+  }
+
+  componentWillUnmount() {
+    let id = window.setTimeout(null, 0);
+    while (id--) {
+      window.clearTimeout(id);
+    }
+  }
+
+  typeToIcon = (type) => {
+    if (type === "success")
+      return Done;
+    if (type === "danger")
+      return ErrorOutline;
+    return null;
+  }
+
+  warning = (msg) => {
+    this.setState({
+      notificationType: "danger",
+      notificationMessage: msg
+    })
+    this.showNotification("br");
+  }
+
+  success = (msg) => {
+    this.setState({
+      notificationType: "success",
+      notificationMessage: msg
+    })
+    this.showNotification("br");
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    let firstChosen = this.state.firstChosen;
+    let secondChosen = this.state.secondChosen;
+    if (!(this.state.firstChosen && this.state.secondChosen)){
+      this.warning(timeChosenMessage);
+      return;
+    }
+    let start;
+    let end;
+    let chosenDate = this.state.chosenDate;
+    if (!secondChosen){
+      start = firstChosen[0];
+      end = start + 1;
+    }
+    else{
+      start = firstChosen[0];
+      end = secondChosen[0]+1;
+    }
+    
+    let meeting = {
+      "attendantNum": null,
+      "attendants": null,
+      "date": chosenDate,
+      "description": "无",
+      "endTime": end,
+      "heading": "Meeting-" + chosenDate + "-" + start + "-" + end ,
+      "hostId": this.props.userId,
+      "location": null,
+      "needSignIn": false,
+      "roomId": this.props.roomId,
+      "startTime": start,
+      "type": "COMMON"
+    }
+    meeting = JSON.stringify(meeting);
+    let api = meetingController.createMeeting();
+    console.log(api)
+    console.log(meeting)
+    fetch(api, {
+      credentials: 'include',
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: meeting
+    })
+    .then( res => res.json())
+    .then((data) => {
+      if (data.error){
+        this.warning(data.error);
+      }
+      else {
+        this.success(reservationSuccessMessage);
+        window.location.reload();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  handleChange = (state) => {
+    this.setState(state);
+    if (state.notificationType)
+      this.showNotification("br");
   }
 
   render(){
     let { roomId, roomLocation} = this.props.match.params;
+    let { firstChosen, secondChosen } = this.state;
+    let timeChosen = "";
+    if (firstChosen && secondChosen){
+      let start;
+      let end;
+      if (firstChosen[0] > secondChosen[0]){
+        start = secondChosen[0];
+        end = firstChosen[0];
+      }
+      else{
+        end = secondChosen[0];
+        start = firstChosen[0];
+      }
+      timeChosen = this.state.chosenDate + " " + idToTime(start) + " ~ " + idToTime(end+1);
+    }
+    else if (firstChosen && !secondChosen){
+      timeChosen = this.state.chosenDate + " " + idToTime(firstChosen[0]) + " ~ " + idToTime(firstChosen[0]+1);
+    }
+    console.log(this.handleChange)
     return(
       <div>
-        <RoomSchedule roomId={roomId} roomLocation={roomLocation} userId={this.props.userId}/>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={12}>
+            <Card>
+              <CardHeader>
+                <h2>{roomLocation}</h2>
+                <table>
+                  <tbody>
+                    <tr>
+                      <td width={220}>
+                        <TextField
+                          fullWidth
+                          disabled
+                          label="预约时间"
+                          value={timeChosen}
+                          margin="normal"
+                          variant="outlined"
+                        />
+                      </td>
+                      <td>
+                        &nbsp;&nbsp;&nbsp;&nbsp;
+                      </td>
+                      <td>
+                        <Fab variant="extended" color="primary" onClick={this.handleSubmit}>
+                          <Done/>
+                          &nbsp;&nbsp;确定预约
+                        </Fab>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </CardHeader>
+              <CardBody>
+                <Paper>
+                  <RoomSchedule roomId={roomId} handleChange={this.handleChange}/>
+                  <Snackbar
+                    place="br"
+                    color={this.state.notificationType}
+                    icon={this.typeToIcon(this.state.notificationType)}
+                    message={this.state.notificationMessage}
+                    open={this.state.br}
+                    closeNotification={() => this.setState({ br: false })}
+                    close
+                  />
+                </Paper>
+              </CardBody>
+            </Card>
+          </GridItem>
+        </GridContainer>
       </div>
     )
   }
