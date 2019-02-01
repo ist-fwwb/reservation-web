8a6c4fa8add5c880c170ea9118f0b0e461a17882
zzbslayer <zzbslayer@sjtu.edu.cn>
2019-02-01 17:28:40 +0800

    show hostname instead of hostid

--- src/views/MeetingPage/MeetingPage.jsx
+++ src/views/MeetingPage/MeetingPage.jsx
@@ -414,7 +414,7 @@ class MeetingPage extends React.Component {
                                   <TableCell align="left">
                                     <Link to={"/meeting/"+ele.id+"/profile"}>{ele.heading?ele.heading: "null"}</Link>
                                   </TableCell>
-                                  <TableCell align="left"><Link to={"/user/"+ele.hostId+"/profile"}>{ele.hostId}</Link></TableCell>
+                                  <TableCell align="left"><Link to={"/user/"+ele.hostId+"/profile"}>{ele.host.name}</Link></TableCell>
                                   <TableCell align="left"><Link to={"/room/"+ele.id+"/profile"}>{ele.location}</Link></TableCell>
                                   <TableCell align="left">{ele.date}</TableCell>
                                   <TableCell align="left">{idToTime(ele.startTime) + "~" + idToTime(ele.endTime)}</TableCell>
